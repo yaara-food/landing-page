b787201f273dbe3ade210d754215b3a1c3e15fca
giladfuchs <igilfu@gmail.com>
2025-05-17 18:38:55 +0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
